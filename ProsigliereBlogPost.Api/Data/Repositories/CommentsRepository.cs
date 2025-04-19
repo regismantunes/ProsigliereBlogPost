474df4ec3fbbf2ef8aef550fcd2440c71041a024
Régis Antunes <65835931+regismantunes@users.noreply.github.com>
2025-04-19 10:58:46 -0400

    Increment BlogPost.CommentCount on adding comments

--- ProsigliereBlogPost.Api/Data/Repositories/CommentsRepository.cs
+++ ProsigliereBlogPost.Api/Data/Repositories/CommentsRepository.cs
@@ -8,6 +8,10 @@ namespace ProsigliereBlogPost.Api.Data.Repositories
     {
         public async Task<int> CreateAsync(Comment comment)
         {
+            var blogPost = await context.BlogPosts.FindAsync(comment.BlogPostId)
+                ?? throw new KeyNotFoundException($"BlogPost with ID {comment.BlogPostId} not found.");
+
+            blogPost.CommentsCount++;
             context.Comments.Add(comment);
             await context.SaveChangesAsync();
             return comment.Id;
