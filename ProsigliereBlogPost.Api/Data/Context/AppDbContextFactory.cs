d2701c7499f10daf047c4fba23ba3e21f4db347d
Régis Antunes <65835931+regismantunes@users.noreply.github.com>
2025-04-19 15:05:10 -0400

    Creating migrations

---
+++ ProsigliereBlogPost.Api/Data/Context/AppDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ProsigliereBlogPost.Api.Extensions;
+
+namespace ProsigliereBlogPost.Api.Data.Context
+{
+    public class AppDbContextFactory() : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>()
+                .UseConfiguration(configuration);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
